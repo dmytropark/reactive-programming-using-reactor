251c138099b2c1942f2c50fbe26285af06845bab
dima <dimagit24@gmail.com>
2023-06-30 16:57:01 +0300

    added tests

--- src/test/java/com/learnreactiveprogramming/BackpressureTest.java
+++ src/test/java/com/learnreactiveprogramming/BackpressureTest.java
@@ -87,4 +87,139 @@ public class BackpressureTest {
                 });
         assertTrue(latch.await(5L, TimeUnit.SECONDS));
     }
+
+    /**
+     * if consumer (subscriber) is slower than producer (source)
+     * we can use onBackpressureDrop mechanism to skip additional messages from source.
+     *
+     */
+    @Test
+    void testBackPressure_drop() throws InterruptedException {
+        var numberRange = Flux.range(1,100).log();
+
+        CountDownLatch latch = new CountDownLatch(1);
+
+        numberRange
+                .onBackpressureDrop(item -> {
+                    log.info("dropped items are: {}", item);
+                })
+                .subscribe(new BaseSubscriber<Integer>() {
+                    @Override
+                    protected void hookOnSubscribe(Subscription subscription) {
+                        request(2);
+                    }
+                    @Override
+                    protected void hookOnNext(Integer value) {
+                        log.info("hookOnNext : {}", value);
+
+                        if (value == 2) {
+                           hookOnCancel();
+                        }
+                    }
+                    @Override
+                    protected void hookOnComplete() {
+                        //super.hookOnComplete();
+                    }
+                    @Override
+                    protected void hookOnError(Throwable throwable) {
+                        //super.hookOnError(throwable);
+                    }
+                    @Override
+                    protected void hookOnCancel() {
+                        // super.hookOnCancel();
+                        log.info("Inside OnCancel");
+                        latch.countDown();
+                    }
+                });
+        assertTrue(latch.await(5L, TimeUnit.SECONDS));
+    }
+
+    /**
+     * We can use buffer approach when producer is too slow in comparison to subscriber (consumer),
+     * so we can preload messages in advance.
+     */
+    @Test
+    void testBackPressure_buffer() throws InterruptedException {
+        var numberRange = Flux.range(1,100).log();
+
+        CountDownLatch latch = new CountDownLatch(1);
+
+        numberRange
+                .onBackpressureBuffer(10, item -> {
+                    log.info("!!! item = {}", item);
+                })
+                .subscribe(new BaseSubscriber<Integer>() {
+                    @Override
+                    protected void hookOnSubscribe(Subscription subscription) {
+                        request(1);
+                    }
+                    @Override
+                    protected void hookOnNext(Integer value) {
+                        log.info("hookOnNext : {}",value);
+
+                        if (value < 3) {
+                            request(1);
+                        } else {
+                            hookOnCancel();
+                        }
+                    }
+                    @Override
+                    protected void hookOnComplete() {
+                        //super.hookOnComplete();
+                    }
+                    @Override
+                    protected void hookOnError(Throwable throwable) {
+                        //super.hookOnError(throwable);
+                    }
+                    @Override
+                    protected void hookOnCancel() {
+                        // super.hookOnCancel();
+                        log.info("Inside OnCancel");
+                        latch.countDown();
+                    }
+                });
+        assertTrue(latch.await(5L, TimeUnit.SECONDS));
+    }
+
+    @Test
+    void testBackPressure_error() throws InterruptedException {
+        var numberRange = Flux.range(1,100).log();
+
+        CountDownLatch latch = new CountDownLatch(1);
+
+        numberRange
+                .onBackpressureError()
+                .subscribe(new BaseSubscriber<>() {
+                    @Override
+                    protected void hookOnSubscribe(Subscription subscription) {
+                        request(1);
+                    }
+
+                    @Override
+                    protected void hookOnNext(Integer value) {
+                        log.info("hookOnNext : {}", value);
+                        hookOnCancel();
+
+                    }
+
+                    @Override
+                    protected void hookOnComplete() {
+                        //super.hookOnComplete();
+                    }
+
+                    @Override
+                    protected void hookOnError(Throwable throwable) {
+                        //super.hookOnError(throwable);
+                        log.error("!!!!", throwable);
+                    }
+
+                    @Override
+                    protected void hookOnCancel() {
+                        // super.hookOnCancel();
+                        log.info("Inside OnCancel");
+                        latch.countDown();
+                    }
+                });
+        assertTrue(latch.await(5L, TimeUnit.SECONDS));
+    }
 }
